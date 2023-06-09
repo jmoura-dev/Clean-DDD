179416e6c7761a6894b88b3428270791a0bcc4d1
Jackson <jackson1712@hotmail.com>
2023-06-09 17:05:30 -0300

    feat: create use case to comment at questions and answers and create tests

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,34 @@
+import { makeAnswer } from "test/factories/make-answer"
+import { InMemoryAnswerCommentsRepository } from "test/repositories/in-memory-answer-comments-repository"
+import { CommentOnAnswerUseCase } from "./comment-on-answer"
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository"
+
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: CommentOnAnswerUseCase
+
+describe('Comment on Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerCommentsRepository,
+    )
+  })
+
+  it('should be able to comment on answer', async () => {
+    const answer = makeAnswer()
+
+    await inMemoryAnswersRepository.create(answer)
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: answer.authorId.toString(),
+      content: 'Test comment',
+    })
+
+    expect(inMemoryAnswerCommentsRepository.items[0].content).toEqual('Test comment')
+  })
+})
